2026efe41468a7bdb111c2015b299330cd05a3f0
mag1cfrog <harrywong2017@gmail.com>
2024-11-03 02:50:21 -0800

    Add ArrayTile class implementing Tile interface with position tracking

---
+++ code/backend/src/implementations/array_tile.py
@@ -0,0 +1,23 @@
+from backend.src.interface.tile import Tile
+
+class ArrayTile(Tile):
+    """
+    Tile implementation using nested lists as 2D array.
+    """
+    def __init__(self, position: tuple, value: int = 2) -> None:
+        self.x, self.y = position
+        self.value = value
+        self.previous_position = None
+        self.merged_from = None # Tracks tiles that merged together
+
+    def save_position(self):
+        self.previous_position = (self.x, self.y)
+
+    def update_position(self, position: tuple):
+        self.x, self.y = position
+
+    def serialize(self):
+        return {
+            "position": (self.x, self.y),
+            "value": self.value,
+        }
