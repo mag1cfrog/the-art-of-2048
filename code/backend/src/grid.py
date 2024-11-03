971d4650d1fcb12482ef4ae60b3c79dab09990b4
mag1cfrog <harrywong2017@gmail.com>
2024-11-03 02:20:01 -0800

    Add abstract Grid class to define grid interface

---
+++ code/backend/src/grid.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+
+class Grid(ABC):
+    """
+    Abstract class to define the interface of a Grid.
+    """
+
+    @abstractmethod
+    def __init__(self, size: int, previous_state=None) -> None:
+        pass
+
+    @abstractmethod
+    def empty(self):
+        pass
+    
+    @abstractmethod
+    def from_state(self, state):
+        pass
+    
+    @abstractmethod
+    def random_available_cell(self):
+        pass
+
+    @abstractmethod
+    def available_cells(self):
+        pass
+
+    @abstractmethod
+    def each_cell(self):
+        pass
+
+    @abstractmethod
+    def cells_available(self):
+        pass
+
+    @abstractmethod
+    def cell_available(self, cell):
+        pass
+
+    @abstractmethod
+    def cell_occupied(self, cell):
+        pass
+
+    @abstractmethod
+    def cell_content(self, cell):
+        pass
+
+    @abstractmethod
+    def insert_tile(self, tile):
+        pass
+
+    @abstractmethod
+    def remove_tile(self, tile):
+        pass
+
+    @abstractmethod
+    def within_bounds(self, position):
+        pass
+
+    @abstractmethod
+    def serialize(self):
+        pass
